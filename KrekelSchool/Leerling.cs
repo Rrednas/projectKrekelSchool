5e1b35615b395dd6c739693fb8e37d9448e20c7f
Dennis <dennisman222@hotmail.com>
2015-02-13 19:01:44 +0100

    klassediagram bijgewerkt

--- KrekelSchool/Leerling.cs
+++ KrekelSchool/Leerling.cs
@@ -8,6 +8,17 @@ namespace KrekelSchool
 {
     public class Leerling
     {
+        public string Naam
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    
         public Uitlening GetUitlening()
         {
             throw new System.NotImplementedException();
@@ -16,7 +27,51 @@ namespace KrekelSchool
 
     public class Gebruiker
     {
-        public Uitlening AddUitlening()
+        public int GebruikerId
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Voornaam
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Achternaam
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public DateTime Geboortedatum
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    
+        public void AddUitlening()
         {
             throw new System.NotImplementedException();
         }
@@ -31,7 +86,7 @@ namespace KrekelSchool
             throw new System.NotImplementedException();
         }
 
-        public void GetItems()
+        public List<Item> GetItems()
         {
             throw new System.NotImplementedException();
         }
@@ -56,7 +111,7 @@ namespace KrekelSchool
             throw new System.NotImplementedException();
         }
 
-        public void GetGebruikers()
+        public List<Gebruiker> GetGebruikers()
         {
             throw new System.NotImplementedException();
         }
@@ -74,17 +129,72 @@ namespace KrekelSchool
 
     public class Uitlening
     {
-        public void GetItem()
+        public void UitleningId
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int UitgeleendAan
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int Startdatum
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int Einddatum
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int UitgeleendItem
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public Item GetItem()
         {
             throw new System.NotImplementedException();
         }
 
-        public void GetLeerling()
+        public Leerling GetLeerling()
         {
             throw new System.NotImplementedException();
         }
 
-        public void BerekenBoete()
+        public int BerekenBoete()
         {
             throw new System.NotImplementedException();
         }
@@ -92,18 +202,267 @@ namespace KrekelSchool
 
     public class Boek : Item
     {
+        public String ISBN
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Titel
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public String Auteur
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int Jaar
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Taal
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Uitgeverij
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Leeftijd
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Thema
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Locatie
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
     }
 
     public class Spel : Item
     {
+        public int SpelId
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Naam
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string AantalSpelers
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Leeftijd
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public String Locatie
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
     }
 
     public class Verteltas : Item
     {
+        public int TasId
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public List<Item> Inhoud
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Locatie
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
     }
 
     public class CD : Item
     {
+        public int CDId
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Naam
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public String Artiest
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int AantalNummers
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Locatie
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string Genre
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
     }
 
     public class Vrijwilliger
