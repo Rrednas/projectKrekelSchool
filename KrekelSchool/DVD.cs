6f368cbf32bef6973d2adc32eee0cae2d7565693
Rrednas <datsander@gmail.com>
2015-02-19 08:48:45 +0100

    classdiagram gefixed

---
+++ KrekelSchool/DVD.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace KrekelSchool
+{
+    public class DVD : Iitem
+    {
+        public int ID
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Naam
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Beschrijving
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool Beschikbaar
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ KrekelSchool/Uitlening.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace KrekelSchool
+{
+    public class Uitlening
+    {
+        public Iitem Iitem
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
