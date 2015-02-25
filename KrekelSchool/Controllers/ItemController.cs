1e33670b4de642163dc38c5f5d02a9bb23af7b27
Mj0lnir87 <rutger.meert.u8598@student.hogent.be>
2015-02-25 15:06:53 +0100

    under construction

--- KrekelSchool/Controllers/ItemController.cs
+++ KrekelSchool/Controllers/ItemController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Services;
 using System.Web.Services.Protocols;
 using System.ComponentModel;
+using KrekelSchool.Models;
 using KrekelSchool.Models.DAL;
 using KrekelSchool.Models.Domain1;
 
@@ -12,50 +16,70 @@ namespace KrekelSchool
 {
     public class ItemController : Controller
     {
-        public Collection<Item> Items = new Collection<Item>();
-        public KrekelSchoolContext Context;
+        //public Collection<Item> Items = new Collection<Item>();
+        //public KrekelSchoolContext Context;
 
-        public void AddItem(string naam, string beschrijving, int beschikbaar)
-        {
-            using (Context = new KrekelSchoolContext())
-            {
-                Context.items.Create();
-            }
-        }
+        //public void AddItem(string naam, string beschrijving, int beschikbaar)
+        //{
+        //    using (Context = new KrekelSchoolContext())
+        //    {
+        //        Context.items.Create();
+        //    }
+        //}
 
-        public Collection<Item> getItems()
-        {
-            return Items;
-        }
+        //public Collection<Item> getItems()
+        //{
+        //    return Items;
+        //}
 
-        public void removeItem(int ID)
-        {
-            Items.RemoveAt(ID);
-        }
+        //public void removeItem(int ID)
+        //{
+        //    Items.RemoveAt(ID);
+        //}
        
-        public void editItem(int ID)
-        {
-            throw new System.NotImplementedException();
-        }
+        //public void editItem(int ID)
+        //{
+        //    throw new System.NotImplementedException();
+        //}
 
-        public Item getItem()
+        //public Item getItem()
+        //{
+        //    throw new System.NotImplementedException();
+        //}
+
+        private IitemRepository itemRepository;
+
+        public ItemController(IitemRepository ir)
         {
-            throw new System.NotImplementedException();
+            itemRepository = ir;
         }
 
         public ActionResult Item()
         {
             ViewBag.Message = "Klik op het gewenste item om deze te bewerken of te verwijderen.";
+            IEnumerable<Item> items =
+                itemRepository.FindAll()
+                    .Include(i => i.Id)
+                    .Include(i => i.Naam)
+                    .Include(i => i.Beschikbaar)
+                    .Include(i => i.Beschrijving);
+            IEnumerable<ItemScreenViewModel> isvm =
+                items.Select(i => new ItemScreenViewModel(i)).OrderBy(i => i.Naam).ToList();
 
-            return View();
+            return View(isvm);
         }
-        public ActionResult ItemScreen()
+        public ActionResult ItemScreen(int id)
         {
             ViewBag.Message = "Geef ID, naam in als zoekcriteria.";
 
             var model = Items;
 
             return View(model);
+        }
+
+        public ActionResult Details(int Id)
+        {
+            return View();
         }
 
     }
---
+++ KrekelSchool/Models/ItemViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using KrekelSchool.Models.Domain1;
+
+namespace KrekelSchool.Models
+{
+    public class ItemViewModel
+    {
+
+        public ItemViewModel(Item item)
+        {
+            Id = item.Id;
+            Naam = item.Naam;
+            Beschrijving = item.Beschrijving;
+            Beschikbaar = item.Beschikbaar;
+        }
+
+
+        public int Id { get; private set; }
+
+        [Display(Name = "Naam")]
+        [Required(ErrorMessage = "{0} is verplicht")]
+        public string Naam { get; private set; }
+
+        [Display(Name = "Beschrijving")]
+        public string Beschrijving { get; private set; }
+
+        [Display(Name = "Aantal beschikbaar")]
+        public int Beschikbaar { get; private set; }
+    }
+    
+    public class ItemScreenViewModel
+    {
+        public ItemScreenViewModel(Item item)
+        {
+            Id = item.Id;
+            Naam = item.Naam;
+            Beschrijving = item.Beschrijving;
+            Beschikbaar = item.Beschikbaar;
+        }
+
+        public int Id { get; private set; }
+
+        [Display(Name = "Naam")]
+        public string Naam { get; private set; }
+
+        [Display(Name = "Beschrijving")]
+        public string Beschrijving { get; private set; }
+
+        [Display(Name = "Aantal beschikbaar")]
+        public int Beschikbaar { get; private set; }
+    }
+}
