377f6c9acb2c800966e68e328bfbf6b2f307d45c
Rrednas <datsander@gmail.com>
2015-02-26 13:03:23 +0100

    Item beter

--- KrekelSchool/Models/Domain1/Item.cs
+++ KrekelSchool/Models/Domain1/Item.cs
@@ -11,15 +11,6 @@ using Microsoft.Ajax.Utilities;
 
 namespace KrekelSchool
 {
-    public enum Soort
-    {
-        cd = 1,
-        dvd = 2,
-        boek = 3,
-        spel = 4,
-        verteltas = 5
-    }
-
     public abstract class Item
     {
         private string id;
@@ -30,28 +21,21 @@ namespace KrekelSchool
 
         private string beschrijving;
 
-        
 
-        private Soort soort;
-
-
-
-        protected Item(string id, Soort soort, string naam, int beschikbaar, string beschrijving)
+        protected Item(string id,  string naam, int beschikbaar, string beschrijving)
         {
             this.id = id;
             this.naam = naam;
             this.beschikbaar = beschikbaar;
             this.beschrijving = beschrijving;
-            this.soort = soort;
         }
 
-        protected Item( Soort soort,string naam, int beschikbaar, string beschrijving)
+        protected Item( string naam, int beschikbaar, string beschrijving)
         {
             
             this.Naam = naam;
             this.Beschikbaar = beschikbaar;
             this.Beschrijving = beschrijving;
-            this.soort = soort;
         }
 
         protected Item()
@@ -59,26 +43,7 @@ namespace KrekelSchool
             
         }
 
-        public string Id
-        {
-            get
-            {
-                string id2 = "";
-                if (soort == Soort.cd)
-                    id2 = string.Format("CD"+ id);
-                if (soort == Soort.spel)
-                    id2 = string.Format("SPEL"+ id);
-                if (soort == Soort.verteltas)
-                    id2 = string.Format("TAS"+ id);
-                if (soort == Soort.dvd)
-                    id2 = string.Format("DVD"+ id);
-                if (soort == Soort.boek)
-                    id2 = string.Format("BOEK"+ id);
-                return id2;
-            }
-            set { }
-        }
-
+       
         public string Naam { get; set; }
 
         public int Beschikbaar { get; set; }
